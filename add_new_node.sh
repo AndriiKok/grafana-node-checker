d5ce0b0fedce41cc48c9bd0eec7f3edde5dcff8e
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-19 13:46:55 +0200

    Update add_new_node.sh

--- add_new_node.sh
+++ add_new_node.sh
@@ -6,44 +6,13 @@ source .profile
 sudo apt-get update
 npm install prom-client fs child_process
 
-# Создаём файл со скриптом проверки ноды
-sudo tee /root/Grafana_node_checker/$file_name.js >/dev/null <<EOF
-const fs = require('fs');
-const { exec } = require('child_process');
-const client = require('prom-client');
+# Скачиваем скрипт с чекером и переименовываем переменные
+curl -sSL https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/checker.js > "/root/Grafana_node_checker/$file_name.js"
 
-const nodeHealthMetric = new client.Gauge({
-  name: '${file_name}',
-  help: '${project} health status'
-});
+sed -i "s=file_name=$file_name=g" /root/Grafana_node_checker/$file_name.js
+sed -i "s=project=$project=g" /root/Grafana_node_checker/$file_name.js
 
-const writeMetricsToFile = async () => {
-  const metrics = await client.register.metrics();
-  fs.writeFileSync('/var/lib/prometheus/node-exporter/${file_name}.prom', metrics);
-  console.log('Metrics written to file: /var/lib/prometheus/node-exporter/${file_name}.prom');
-};
-
-const checkHealth = async () => {
-  exec('curl -X GET http://localhost:8645/health', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing script: ${error}`);
-      nodeHealthMetric.set(0);
-      console.log('Node health status: Not Ready (Error executing script)');
-    } else if (stdout.includes('Ready')) {
-      nodeHealthMetric.set(1);
-      console.log('Node health status: Ready');
-    } else {
-      nodeHealthMetric.set(0);
-      console.log('Node health status: Not Ready');
-    }
-    writeMetricsToFile();
-  });
-};
-
-checkHealth();
-EOF
-
-# Получаем абсолютный путь к node
+# Получаем абсолютный путь к nodejs
 node_path=$(which node)
 
 # Добавляем файл в крон с частотой выполнения каждую минуту
