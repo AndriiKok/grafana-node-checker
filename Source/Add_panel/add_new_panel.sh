77db71a8dbaeff89cb0424abc9e2b38b17a7fefd
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-20 22:21:27 +0200

    Update add_new_panel.sh

--- Source/Add_panel/add_new_panel.sh
+++ Source/Add_panel/add_new_panel.sh
@@ -43,11 +43,11 @@ npm install axios
 
 # Запуск файла add_new_panel.js с помощью Node.js
 echo "Создаём новую панель для вашего дашборда..."
-$(which node) /root/adding_panel/add_new_panel.js
+PANEL_UID=$($(which node) /root/adding_panel/add_new_panel.js | grep 'PANEL_UID' | awk -F': ' '{print $2}')
 
 # Проверка успешности выполнения скрипта
-if [ $? -eq 0 ]; then
-  echo "Готово, проверяйте свой дашборд"
+if [ -n "$PANEL_UID" ]; then echo "Готово, проверяйте свой дашборд" 
+echo "PANEL_UID=${PANEL_UID}" >> ~/.profile
 else
   sed -i '/export GRAFANA_API_KEY=/d' ~/.profile
   sed -i '/export DASHBOARD_UID=/d' ~/.profile
