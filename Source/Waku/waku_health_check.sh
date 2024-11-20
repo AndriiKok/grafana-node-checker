df42179b938173189c7c58be35653f9691aca862
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-20 17:47:55 +0200

    Update and rename add_new_node.sh to Source/Waku/waku_health_check.sh

--- Source/Waku/waku_health_check.sh
+++ Source/Waku/waku_health_check.sh
@@ -1,23 +1,17 @@
-read -p "Введите название проекта, который нужно добавить: " project
-file_name="${project}_health_checker"
-
 mkdir -p /root/Grafana_node_checker
 source .profile
 sudo apt-get update
 npm install prom-client fs child_process
 
-# Скачиваем скрипт с чекером и переименовываем переменные
-curl -sSL https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/checker.js > "/root/Grafana_node_checker/$file_name.js"
-
-sed -i "s=file_name=$file_name=g" /root/Grafana_node_checker/$file_name.js
-sed -i "s=project=$project=g" /root/Grafana_node_checker/$file_name.js
+# Скачиваем скрипт с чекером
+curl -sSL https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/Source/Waku/waku_health_check.js > "/root/Grafana_node_checker/waku_health_check.js"
 
 # Получаем абсолютный путь к nodejs
 node_path=$(which node)
 
 # Добавляем файл в крон с частотой выполнения каждую минуту
 source .profile
-cron_entry="* * * * * $node_path /root/Grafana_node_checker/$file_name.js"
+cron_entry="* * * * * $node_path /root/Grafana_node_checker/waku_health_check.js"
 sudo crontab -l | { cat; echo "$cron_entry"; } | sudo crontab -
 
 # Добавляем в сервисник Node Exporter ключ для запуска сервиса с папкой с новой метрикой
