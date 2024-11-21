aca9426c7eb679e8bf313e14cafe60d15dd2f2f9
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-21 12:35:09 +0200

    Update Reset_dashboard.sh

--- Source/Reset_dashboard.sh
+++ Source/Reset_dashboard.sh
@@ -33,18 +33,19 @@ echo "Значения успешно добавлены или загружен
 
 # Указываем необходимые переменные
 json_url="https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/json_model_original.json"
-dashboard_api_url="http://GRAFANA_SERVER_IP:3002/api/dashboards/uid/${DASHBOARD_UID}"
+dashboard_api_url="http://${GRAFANA_SERVER_IP}:3002/api/dashboards/uid/${DASHBOARD_UID}"
+api_url="http://${GRAFANA_SERVER_IP}:3002/api/dashboards/db"
 
 # Получаем текущий дашборд
-dashboard_data=$(curl -s -H "Authorization: Bearer $GRAFANA_API_KEY" $dashboard_api_url)
+dashboard_data=$(curl -s -H "Authorization: Bearer $GRAFANA_API_KEY" "$dashboard_api_url")
 
 # Извлекаем title, uid и version
-current_title=$(echo $dashboard_data | jq -r '.dashboard.title')
-current_uid=$(echo $dashboard_data | jq -r '.dashboard.uid')
-current_version=$(echo $dashboard_data | jq -r '.dashboard.version')
+current_title=$(echo "$dashboard_data" | jq -r '.dashboard.title')
+current_uid=$(echo "$dashboard_data" | jq -r '.dashboard.uid')
+current_version=$(echo "$dashboard_data" | jq -r '.dashboard.version')
 
 # Скачиваем JSON модель дашборда
-curl -sSL $json_url -o /tmp/json_model_original.json
+curl -sSL "$json_url" -o /tmp/json_model_original.json
 
 # Обновляем значения title, uid и version в JSON модели
 jq --arg title "$current_title" --arg uid "$current_uid" --arg version "$current_version" '.dashboard.title = $title | .dashboard.uid = $uid | .dashboard.version = ($version | tonumber)' /tmp/json_model_original.json > /tmp/json_model_updated.json
@@ -56,7 +57,7 @@ json_data=$(cat /tmp/json_model_updated.json)
 update_payload=$(jq -n --argjson dashboard "$json_data" '{dashboard: $dashboard, overwrite: true}')
 
 # Отправляем запрос на обновление дашборда
-response=$(curl -s -X POST $api_url -H "Authorization: Bearer $GRAFANA_API_KEY" -H "Content-Type: application/json" -d "$update_payload")
+response=$(curl -s -X POST "$api_url" -H "Authorization: Bearer $GRAFANA_API_KEY" -H "Content-Type: application/json" -d "$update_payload")
 
 # Проверка результата
 if [[ $response == *"success"* ]]; then
