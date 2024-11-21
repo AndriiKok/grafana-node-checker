f3b810a7a4ad5e9d624358b4f9a44686688815ed
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-21 14:22:24 +0200

    Update waku_peers_add_column.js

--- Source/Waku/waku_peers_add_column.js
+++ Source/Waku/waku_peers_add_column.js
@@ -26,7 +26,7 @@ const newStyle = {
     }
 
 const newTarget = {
-  expr: "waku_peers_check",
+  expr: "waku_peers_check{origin_prometheus=~"$origin_prometheus",job=~"$job"} - 0",
   format: "table",
   instant: true,
   refId: "B"
