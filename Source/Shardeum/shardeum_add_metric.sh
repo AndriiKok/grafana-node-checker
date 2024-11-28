7ccc6a372e2ae5d8348ee98659667ba4bf15cad3
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-28 20:01:16 +0200

    Update shardeum_add_metric.sh

--- Source/Shardeum/shardeum_add_metric.sh
+++ Source/Shardeum/shardeum_add_metric.sh
@@ -4,15 +4,15 @@
 mkdir -p /root/Grafana_node_checker
 source ~/.profile
 
-# Скачиваем скрипт с чекером
-curl -sSL https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/Source/Shardeum/shardeum_status_check.sh > "/root/Grafana_node_checker/shardeum_status_check.sh"
+if ! command -v screen &> /dev/null; then
+    sudo apt update && sudo apt install screen -y 2>&1
+fi
 
-# Добавляем файл в крон с частотой выполнения каждые 15 минут
-cron_entry="*/15 * * * * bash /root/Grafana_node_checker/shardeum_status_check.sh"
-(crontab -l 2>/dev/null; echo "$cron_entry") | crontab -
-
-# Запускаем скрипт для проверки метрики
-bash /root/Grafana_node_checker/shardeum_status_check.sh
+SESSION_NAME="shardeum"
+screen -dmS "$SESSION_NAME"
+sleep 1
+screen -S "$SESSION_NAME" -X stuff "bash <(curl -s https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/Source/Shardeum/shardeum_status_check.sh)"
+screen -S "$SESSION_NAME" -X stuff $'\n'
 
 # Добавляем в сервисник Node Exporter ключ для запуска сервиса с папкой с новой метрикой
 if ! grep -q -- "--collector.textfile.directory=/var/lib/prometheus/node-exporter" /lib/systemd/system/prometheus-node-exporter.service; then
