b1b3796ec298c39900837559ab4c070f81287767
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-29 14:25:59 +0200

    Update hemi_balance_check.js

--- Source/Hemi/hemi_balance_check.js
+++ Source/Hemi/hemi_balance_check.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { exec } = require('child_process');
 const client = require('prom-client');
 const https = require('https');
 
@@ -12,51 +11,6 @@ const writeMetricsToFile = async () => {
   const metrics = await client.register.metrics();
   fs.writeFileSync('/var/lib/prometheus/node-exporter/hemi_balance_check.prom', metrics);
   console.log('Metrics written to file: /var/lib/prometheus/node-exporter/hemi_balance_check.prom');
-};
-
-const getNodeID = async () => {
-  return new Promise((resolve, reject) => {
-    const profilePath = `${process.env.HOME}/.profile`;
-    fs.readFile(profilePath, 'utf8', (err, data) => {
-      if (err || !data.includes('node_id=')) {
-        // Перезапускаем службу hemi и ждем 10 секунд
-        exec("sudo systemctl restart hemi", (err) => {
-          if (err) {
-            console.error(`Ошибка при перезапуске службы hemi: ${err.message}`);
-            return resolve(null);
-          }
-
-          console.log("Перезапуск службы hemi выполнен. Ожидайте, пожалуйста...");
-
-          setTimeout(() => {
-            // Выполняем команду для получения node_id
-            exec("journalctl -n 50 -u hemi -o cat | grep -oP '(?<=address )[^\s]+' | cut -d ' ' -f 1", (err, stdout, stderr) => {
-              if (err) {
-                console.error(`Ошибка при получении node_id: ${err.message}`);
-                return resolve(null);
-              }
-
-              console.log("Данные получены. Ожидайте, пожалуйста...");
-
-              // Подождем 5 секунд перед продолжением
-              setTimeout(() => {
-                // Разделяем stdout по строкам и берем последнее значение
-                const node_ids = stdout.trim().split('\n');
-                const node_id = node_ids[node_ids.length - 1];
-
-                // Записываем node_id в .profile
-                fs.appendFileSync(profilePath, `\nnode_id=${node_id}`);
-                resolve(node_id);
-              }, 30000); // Ждем 5 секунд
-            });
-          }, 30000); // Ждем 10 секунд
-        });
-      } else {
-        const node_id = data.match(/node_id=([^\n]+)/)[1];
-        resolve(node_id);
-      }
-    });
-  });
 };
 
 const checkBalance = async (node_id) => {
@@ -97,13 +51,9 @@ const checkBalance = async (node_id) => {
 
 const main = async () => {
   try {
-    const node_id = await getNodeID();
-    if (node_id) {
-      await checkBalance(node_id);
-      await writeMetricsToFile();
-    } else {
-      console.error('Не удалось получить node_id');
-    }
+    const node_id = 'your_node_id_here'; // Укажите ваш node_id здесь
+    await checkBalance(node_id);
+    await writeMetricsToFile();
   } catch (error) {
     console.error(`Ошибка в основной функции: ${error.message}`);
   }
