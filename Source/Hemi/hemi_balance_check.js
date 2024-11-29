0e42b623da55c148ff74ecb7de274bdb1e0745c0
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-29 12:47:00 +0200

    Update hemi_balance_check.js

--- Source/Hemi/hemi_balance_check.js
+++ Source/Hemi/hemi_balance_check.js
@@ -24,10 +24,13 @@ const getNodeID = async () => {
             console.error(`Ошибка при получении node_id: ${err.message}`);
             return resolve(null);
           }
-          // Извлекаем только нужное значение node_id
-          const node_id = stdout.trim();
-          fs.appendFileSync(profilePath, `\nnode_id=${node_id}`);
-          resolve(node_id);
+          
+          // Подождем 5 секунд перед продолжением
+          setTimeout(() => {
+            const node_id = stdout.trim();
+            fs.appendFileSync(profilePath, `\nnode_id=${node_id}`);
+            resolve(node_id);
+          }, 5000);
         });
       } else {
         const node_id = data.match(/node_id=([^\n]+)/)[1];
@@ -36,6 +39,7 @@ const getNodeID = async () => {
     });
   });
 };
+
 
 const checkBalance = async (node_id) => {
   const url = `https://mempool.space/testnet/api/address/${node_id}`;
