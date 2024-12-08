b7fbbd6d0d7de986b71518b15792682f35b308bd
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-12-08 15:24:27 +0200

    Update and rename dill_balance_check.sh to dill_balance_1h_check.sh

--- Source/Dill/dill_balance_1h_check.sh
+++ Source/Dill/dill_balance_1h_check.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 npm install prom-client fs child_process
 
 # Скачиваем скрипт с чекером
-curl -sSL https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/Source/Dill/dill_balance_check.js > "/root/Grafana_node_checker/dill_balance_check.js"
+curl -sSL https://raw.githubusercontent.com/AndriiKok/grafana-node-checker/refs/heads/main/Source/Dill/dill_balance_1h_check.js > "/root/Grafana_node_checker/dill_balance_1h_check.js"
 
 # Получаем абсолютный путь к nodejs
 node_path=$(which node)
@@ -13,12 +13,14 @@ node_path=$(which node)
 pubkey=$(find /root/dill/validator_keys -name 'deposit_data-*.json' -exec jq -r '.[0].pubkey' {} \;)
 
 # Заменяем PUB_KEY в dill_balance_check.js на значение из переменной pubkey
-sed -i "s/PUB_KEY/$pubkey/" /root/Grafana_node_checker/dill_balance_check.js
+sed -i "s/PUB_KEY/$pubkey/" /root/Grafana_node_checker/dill_balance_1h_check.js
 
 # Добавляем файл в крон с частотой выполнения каждую минуту
 source .profile
-#cron_entry="* * * * * $node_path /root/Grafana_node_checker/dill_balance_check.js"
-#sudo crontab -l | { cat; echo "$cron_entry"; } | sudo crontab -
+cron_entry="0 * * * * $node_path /root/Grafana_node_checker/dill_balance_1h_check.js"
+sudo crontab -l | { cat; echo "$cron_entry"; } | sudo crontab -
+
+$node_path /root/Grafana_node_checker/dill_balance_1h_check.js
 
 # Добавляем в сервисник Node Exporter ключ для запуска сервиса с папкой с новой метрикой
 if ! grep -q -- "--collector.textfile.directory=/var/lib/prometheus/node-exporter" /lib/systemd/system/prometheus-node-exporter.service; then
