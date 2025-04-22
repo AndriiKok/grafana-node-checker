d37000ef0ffe76d5f047872f2bcc33373877cdbc
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2025-04-22 12:47:36 +0300

    Update ritual_health_check.js

--- Source/Ritual/ritual_health_check.js
+++ Source/Ritual/ritual_health_check.js
@@ -1,34 +1,51 @@
 const fs = require('fs');
+const util = require('util');
 const { exec } = require('child_process');
 const client = require('prom-client');
 
+const execAsync = util.promisify(exec);
+
+// Создание метрики
 const nodeHealthMetric = new client.Gauge({
   name: 'ritual_health_check',
   help: 'Ritual health checker'
 });
 
+// Функция записи метрик в файл с обработкой ошибок
 const writeMetricsToFile = async () => {
-  const metrics = await client.register.metrics();
-  fs.writeFileSync('/var/lib/prometheus/node-exporter/ritual_health_check.prom', metrics);
-  console.log('Metrics written to file: /var/lib/prometheus/node-exporter/ritual_health_check.prom');
+  try {
+    const metrics = await client.register.metrics();
+    console.log('Metrics generated:', metrics);
+
+    const filePath = '/var/lib/prometheus/node-exporter/ritual_health_check.prom';
+    fs.writeFileSync(filePath, metrics);
+    console.log(`Metrics written successfully to: ${filePath}`);
+  } catch (error) {
+    console.error('Error writing metrics:', error);
+  }
 };
 
+// Функция проверки состояния узла
 const checkHealth = async () => {
-  exec('curl 0.0.0.0:4321/health', 
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing script: ${error}`);
-      nodeHealthMetric.set(0);
-      console.log('Node health status: Not OK (Error executing script)');
-    } else if (stdout.includes('healthy')) {
+  try {
+    const { stdout } = await execAsync('curl 0.0.0.0:4321/health');
+
+    if (stdout.includes('healthy')) {
       nodeHealthMetric.set(1);
       console.log('Node health status: OK');
     } else {
       nodeHealthMetric.set(0);
       console.log('Node health status: Not OK');
     }
-    writeMetricsToFile();
-  });
+  } catch (error) {
+    console.error(`Error executing health check: ${error}`);
+    nodeHealthMetric.set(0);
+    console.log('Node health status: Not OK (Error executing script)');
+  }
+
+  // Гарантированная запись метрик после проверки
+  await writeMetricsToFile();
 };
 
+// Запуск функции
 checkHealth();
